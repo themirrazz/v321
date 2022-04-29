863f091e274fce5a337de531761cc10a634f8a7c
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-04-29 08:31:14 -0700

    Update base.js

--- base.js
+++ base.js
@@ -1,19 +1,100 @@
-(async function(fp){
+(async function(fp,op){
+var selfCmd="";
 var fakewin={
   toString:function(){},
-  util:{},
+  util:{wait:function(ms){return new Promise(function(y,n){setTimeout(function(){y()},ms);})},
+  FSUtil:{},
+  fstype:{},
   ui:{
-    get MsgBoxSimple(){return w96.ui.commdlg.msgboxSimple}
+    MsgBoxSimple:
+      error:function(){
+        return w96.ui.commdlg.msgboxSimple.error(...arguments);
+      },
+      info:function(){
+        return w96.ui.commdlg.msgboxSimple.info(...arguments);
+      },
+      warning:function(){
+        return w96.ui.commdlg.msgboxSimple.warning(...arguments);
+      },
+      status:function(){
+        return w96.ui.commdlg.msgboxSimple.status(...arguments);
+      },
+      confirm:function(a,b,c){
+        return w96.ui.commdlg.confirm("ask.???",a,c||'question',undefined,function(){b(true);});
+      },
+      prompt:function(){
+        return w96.ui.commdlg.prompt(...arguments);
+      },
+    },
+    Theme:{
+      getIconUrl:async function getIconUrl(icon){
+        return w96.desktop_shell.getIconUrl(icon);
+      }
+    },
+    DialogCreator:{alert:V3Alert}
+  },
+  WindowParams: class WindowParams extends Object{
+    constructor(a,b,c,d,e){
+      super(a,b,c,d,e)
+      this.title="Untitled Window";
+      this.body="this is where ur crao goes"
+    }
   },
   shell: {
-    mkShortcut: function(xUNUSED_1,icon,cmd) {
-      w96.desktop_shell.addIcon(
-        cmd,
+    mkShortcut: function(path,icon,cmd) {
+      w96.desktop_shell.createShortcut(
+        path,
         icon,
-        20,
-        20,
-        function(){w96.ui.commdlg.msgboxSimple.error("Error", "You are doing this wrong.", 'wat')}
-      )
+        cmd
+       )
+    }
+  },
+  sys:{
+    reboot:function(){location.reload()},
+    renderBSOD:function(message){
+      w96.bsod(message)
+    },
+    execCmd: async function (cmd,args){
+      args=args||[]
+      if(selfCmd&&cmd==selfCmd){
+    var f$=eval(`(async function(w96,WApplication,StandardWindow,FS,alert){
+${await fakewin.FS.readstr(fp)}
+})`);
+f$.call({
+  boxedEnv:{args:[cmd,...args]}
+},fakewin,fakewin.WApplication,fakewin.StandardWindow,fakewin.FS,V3Alert).then(console.info).catch(function(e){
+  alert(e.name+":"+e.message);
+});
+      } else if(cmd==="md") {
+        w96.apps_builtin.markdownViewer.start(
+          args[0].slice(2),
+          fs.resolveByPrefix(args[0][0].toLowerCase()+":")
+        );
+      } else if(cmd=="img") {
+        w96.apps_builtin.imageViewer.start(
+          args[0].slice(2),
+          fs.resolveByPrefix(args[0][0].toLowerCase()+":")
+        );
+      } else if(cmd=="html"){
+        try {
+          new URL(args[0]);
+          w96.apps_builtin.htmlViewer.start(args[0],"SaulGoodMan.com",true)
+        }catch(e){
+          w96.apps_builtin.markdownViewer.start(
+          args[0].slice(2),
+          fs.resolveByPrefix(args[0][0].toLowerCase()+":")
+        );
+        }
+      } else if(cmd=="explorer") {
+        w96.apps_builtin.explorer.start(
+          args[0].slice(2),
+          fs.resolveByPrefix(args[0][0].toLowerCase()+":")
+        );
+      } else if(fakewin.FS.exists(args[0])){
+        w96.execFile(args[0].slice(2),fs.resolveByPrefix(args[0][0].toLowerCase()+":"))
+      } else {
+        w96.desktop_shell.exec("win96://"+cmd);
+      }
     }
   }
 };
@@ -36,20 +117,143 @@ var fakewin={
   
 var SomeV3App=class {
   constructor(){
+    this.windows=[]
     console.log("my app is awesomeyeeeeee");
   }
   async main(argv) {
     console.log("ARRRRRRGH! "+argv);
   }
+  createWindow(params){
+    var sw=new fakewin.StandardWindow(params);
+    this.windows.push(sw);
+    return sw
+  }
 }
 
+
+var $BaseFS=function BaseFileSystem(prefix) {
+  this.prefix=prefix
+  var fs$=new fileSystem(prefix,false);
+  this.selfFs=fs$;
+  this.driverName="idbfs";
+  this.remote=false;
+  this.readOnly=false;
+  Object.defineProperty(this,'volumeLabel',{
+    get:function(){
+      return this.selfFS.label
+    },
+    set:function(bruh){
+      this.selfFS.label=bruh
+    }
+  })
+}
+
+
+
+
+
+
+var $IdbFS=function IndexedFileSystem(prefix) {
+  this.prefix=prefix
+  var fs$=new baseFileSystem(prefix,false);
+  this.selfFs=fs$;
+  this.driverName="idbfs";
+  this.remote=false;
+  this.readOnly=false;
+  Object.defineProperty(this,'volumeLabel',{
+    get:function(){
+      return this.selfFs.label
+    },
+    set:function(bruh){
+      this.selfFs.label=bruh
+    }
+  })
+}
+
+var $ZipFS=function ZipReadOnlyFileSystem(prefix,path) {
+  this.prefix=prefix
+  var that=this
+  fakewin.FS.readbin(path).then(async function(bin){
+    var def=new JSZip();
+    var zip=await def.loadAsync(bin);
+    var k=Object.keys(zip.files);
+    var yT={};
+    for(var n=0;n<k.length;n++){
+      var p=k[n];
+      var f=zip.files[p];
+      yT['/'+p]="%:BIN"+btoa(Uint8ToString(await f.async('uint8array')))
+    }
+  var fs$=new readOnlyFileSystem(prefix,{fs:yT});
+  that.selfFs=fs$;})
+  this.driverName="idbfs";
+  this.remote=false;
+  this.readOnly=false;
+  Object.defineProperty(this,'volumeLabel',{
+    get:function(){
+      return this.selfFs.label
+    },
+    set:function(bruh){
+      this.selfFs.label=bruh
+    }
+  })
+}
+
+
+var $LocalFS=function LocalStorageFileSystem(prefix) {
+  this.prefix=prefix
+  var fs$=new fileSystem(prefix,false);
+  this.selfFs=fs$;
+  this.driverName="localfs";
+  this.remote=false;
+  this.readOnly=false;
+  Object.defineProperty(this,'volumeLabel',{
+    get:function(){
+      return this.selfFs.label
+    },
+    set:function(bruh){
+      this.selfFs.label=bruh
+    }
+  })
+}
+
+
+var $RamFS=function RamFileSystem(prefix) {
+  this.prefix=prefix
+  var fs$=new ramFileSystem(prefix,false);
+  this.selfFs=fs$;
+  this.driverName="ramfs";
+  this.remote=false;
+  this.readOnly=false;
+  Object.defineProperty(this,'volumeLabel',{
+    get:function(){
+      return this.selfFs.label
+    },
+    set:function(bruh){
+      this.selfFs.label=bruh
+    }
+  })
+}
+
+fakewin.fstype.RamFileSystem=$RamFS
+fakewin.fstype.IndexedFileSystem=$IdbFS
+  fakewin.fstype.ZipReadOnlyFileSystem=$ZipFS
+  fakewin.fstype.LocalStorageFileSystem=$LocalFS
+  fakewin.fstype.BaseFileSystem=$BaseFS
 SomeV3App.execAsync=function(a,b){
-  eval(`(async function(w96,SomeV3App,argv){
+  eval(`(async function(w96,WApplication,argv ,StandardWindow,FS,alert){
    a.main(argv);
-})`)(fakewin,SomeV3App,b);
+})`)(fakewin,SomeV3App,b,fakewin.StandardWindow,fakewin.FS,V3Alert);
 }
- fakewin.WApplication=SomeV3App;
-
+ fakewin.WApplication=SomeV3App
+fakewin.WRT={  
+run:async function(a,b){
+  eval(`(async function(w96,WApplication,argv ,StandardWindow,FS,alert){
+   (function(current,env){
+${a}
+}).call(argv,argv,argv.boxedEnv)
+})`)(fakewin,SomeV3App,b,fakewin.StandardWindow,fakewin.FS,V3Alert);
+}
+}
   
 function V3ContextMenu(items) {
   this.$$DEFAULTITEMS$$=items;
@@ -57,11 +261,38 @@ function V3ContextMenu(items) {
 }
  
  function mysafeeval(f,argv){
-  eval(`(async function(w96,SomeV3App,argv){
+  eval(`(async function(w96,WApplication,argv){
    f()
 })`)(fakewin,SomeV3App,f,argv||[]);
  };
   
+  
+  function V3Alert(msg,bruh){
+    var args={title:'Alert',icon:'info',buttons:[{text:'OK',onclick:function(){mbx.closeDialog()}}]}
+    try{args.title=bruh.title||args.title;args.icon=bruh.icon||args.icon;args.buttons=bruh.buttons||args.buttons}
+    catch(e){null}
+    var mbx=new w96.ui.commdlg.MessageBox(
+      args.title,
+      msg,
+      args.icon,
+      "OK",
+      null
+    );
+    var bta=mbx.dlg.window.querySelector('.mbox-buttonsgroup');
+    bta.innerHTML="";
+    var ob={close:function(){mbx.dlg.close()},get wnd(){return w96.util.Swgen(mbx.dlg)},params:args,}
+    for(var i=0;i<args.buttons.length;i++){
+      var bte=document.createElement('button');
+      bte.className='w96-button clicky-thingy ported-v321-butt';
+      bte.innerText=args.buttons[i].text;
+      (function (y,rg){
+      y.onclick=function(){rg.onclick.call(ob)}
+      })(bte,args.buttons[i])
+      bta.appendChild(bte)
+    }
+    mbx.dlg.show()
+    return ob
+  }
   
 V3ContextMenu.prototype.renderMenu=function(x,y,opitems){
   var items=opitems||this.$$DEFAULTITEMS$$;
@@ -99,24 +330,52 @@ V3ContextMenu.prototype.renderMenu=function(x,y,opitems){
   },100);
   return menudiv
 }
-  
+  fakewin.ui.ContextMenu=V3ContextMenu;
 /*
 Todo: convert the V1 fs to V2
 */
 
 fakewin.FS={
+  mounts:function(){
+    var mks=Object.keys(fs);
+    var arr=[]
+    for(var i=0;i<mks.length;i++){
+      var mk=mks[i];
+      if(mk!==undefined){
+      if(mk!=="resolveByPrefix"){
+        var cfs=fs[mk];
+        if(cfs instanceof ramFileSystem){
+          var bruh=new fakewin.fstype.RamFileSystem(cfs.prefix);
+        } else {
+          var bruh=new fakewin.fstype.IndexedFileSystem(cfs.prefix);
+        }
+        bruh.selfFs=cfs;
+        arr.push(bruh);
+      }
+      }else{arr.push(null)}
+    }
+    return arr
+  },
+  mount:function(drive){
+    if(fs.resolveByPrefix(drive.prefix)){return}
+    var fsid=0;
+    while(true){
+      if(!fs["v3Port-Mounted-FS-"+fsid]) {break}
+      fsid++;
+    }
+    fs['v3Port-Mounted-FS-'+fsid]=drive.selfFs;
+  },
   readbin:async function (path){
     var volumeletter=path[0];
-    var dev;
     var AllowedVolumes=['a', 'c'];
-    var Volumes={"a":'floppy_a',"c":'main'};
-    if(AllowedVolumes.indexOf(volumeletter.toLowerCase())==-1){
+    var volumes={'a':'floppy_a','c':'main'};
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
       throw {
         name: 'FSErrno',
         message:'ENODEV: '+path.slice(2)+': No such device'
       }
     }
-    dev=Volumes[path[0]];
     if(!fs[dev].fileExists(path.slice(2))){
        throw {
          name:'FSErrno',
@@ -129,24 +388,26 @@ fakewin.FS={
         message: 'ENONT: '+path.slice(2)+": Is a directory"
       }
     }
-    var str=fs.getFileContents(path.slice(2));
-    if(str.indexOf("%:BIN")==0){
-      str=str.slice(5);
+    var str=fs[dev].getFileContents(path.slice(2));
+    if(this.isBinary(path)){
       str=atob(str);
     }
     var arr=[];
     for(var i=0;i<str.length;i++){
       arr.push(str.charCodeAt(i));
     }
+    window.console.log(arr)
     return new Uint8Array(arr);
   },
+  isBinary:function path(e){
+    return String(fs[fs.resolveByPrefix(e[0].toLowerCase()+":")].storage.getItem(e)).slice(0,5)=="%:BIN"
+  },
   readstr:async function (path){
-    var blob=new Blob([this.readbin(path).buffer]);
-    return fs.main.getFileContents(path.slice(2));
+    var blob=new Blob([(await this.readbin(path)).buffer]);
     return await blob.text();
   },
   toBlob:async function (path){
-    var blob=new Blob([this.readbin(path).buffer]);
+    var blob=new Blob([(await this.readbin(path)).buffer]);
     return blob;
   },
   readdir:async function(path){
@@ -154,16 +415,15 @@ fakewin.FS={
     var volumeletter=path[0];
     var AllowedVolumes=['a', 'c'];
     var volumes={'a':'floppy_a','c':'main'};
-    var dev="";
-    if(AllowedVolumes.indexOf(volumeletter.toLowerCase())==-1){
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
       throw {
         name: 'FSErrno',
         message:'ENODEV: '+path.slice(2)+': No such device'
       }
     };
-    dev=volumes[volumeletter]||"main";
     for(var i=0;i<localStorage.length;i++){
-      if(path==fs[dev].getParentPath(localStorage.key(i).slice(2))){
+      if(path==volumeletter+":"+fs[dev].getParentPath(localStorage.key(i).slice(2))){
          res.push(localStorage.key(i))
       }
     }
@@ -173,19 +433,166 @@ fakewin.FS={
     var volumeletter=path[0];
     var AllowedVolumes=['a', 'c'];
     var volumes={'a':'floppy_a','c':'main'};
-    var dev="";
-    if(AllowedVolumes.indexOf(volumeletter.toLowerCase())==-1){
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
       return false
     };
-    dev=volumes[volumeletter]||"main";
     if(fs[dev].fileExists(path.slice(2))){
        return true;
     };
     if(fs[dev].dirExists(path.slice(2))){
        return true;
     };
-    return true
   },
+  mkdir:async function(path){
+    var volumeletter=path[0];
+    var AllowedVolumes=['a', 'c'];
+    var volumes={'a':'floppy_a','c':'main'};
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
+      throw {
+        name: 'FSErrno',
+        message:'ENODEV: '+path.slice(2)+': No such device'
+      }
+    }
+    fs[dev].createNewFolder(path.slice(2))
+  },
+  writestr: async function(path,data){
+    var arr=[];
+    for(var i=0;i<data.length;i++){
+      arr.push(data.charCodeAt(i));
+    }
+    this.writebin(path,arr);
+  },
+  writebin: async function(path,data){
+    var volumeletter=path[0];
+    var AllowedVolumes=['a', 'c'];
+    var volumes={'a':'floppy_a','c':'main'};
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
+      throw {
+        name: 'FSErrno',
+        message:'ENODEV: '+path.slice(2)+': No such device'
+      }
+    }
+    if(!this.exists(path)){this.touch(path)}
+    fs[dev].writeBinaryFile(path.slice(2),new Uint8Array(data.buffer||data));
+  },
+  touch: async function(path){
+    var volumeletter=path[0];
+    var AllowedVolumes=['a', 'c'];
+    var volumes={'a':'floppy_a','c':'main'};
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
+      throw {
+        name: 'FSErrno',
+        message:'ENODEV: '+path.slice(2)+': No such device'
+      }
+    }
+    fs[dev].createEmptyFile(path);
+  },
+  cpfile:async function(path,dest){
+    var volumeletter=path[0];
+    var AllowedVolumes=['a', 'c'];
+    var volumes={'a':'floppy_a','c':'main'};
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
+      throw {
+        name: 'FSErrno',
+        message:'ENODEV: '+path.slice(2)+': No such device'
+      }
+    }
+    fs[dev].writeFile(
+      dev.slice(2),
+      fs[dev].storage.getItem(path),
+    )
+  },
+  cpdir:async function(path,dest){
+    var volumeletter=path[0];
+    var AllowedVolumes=['a', 'c'];
+    var volumes={'a':'floppy_a','c':'main'};
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
+      throw {
+        name: 'FSErrno',
+        message:'ENODEV: '+path.slice(2)+': No such device'
+      }
+    }
+    fs[dev].copyDir(
+      dev.slice(2),
+      dest.slice(2),
+    )
+  },
+  mvdir:async function(path,dest){
+    var volumeletter=path[0];
+    var AllowedVolumes=['a', 'c'];
+    var volumes={'a':'floppy_a','c':'main'};
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
+      throw {
+        name: 'FSErrno',
+        message:'ENODEV: '+path.slice(2)+': No such device'
+      }
+    }
+    fs[dev].moveDir(
+      dev.slice(2),
+      dest.slice(2),
+    )
+  },
+  mvfile:async function(path,dest){
+    var volumeletter=path[0];
+    var AllowedVolumes=['a', 'c'];
+    var volumes={'a':'floppy_a','c':'main'};
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
+      throw {
+        name: 'FSErrno',
+        message:'ENODEV: '+path.slice(2)+': No such device'
+      }
+    }
+    fs[dev].moveFile(
+      dev.slice(2),
+      dest.slice(2),
+    )
+  },
+  rm:async function(path,dest){
+    var volumeletter=path[0];
+    var AllowedVolumes=['a', 'c'];
+    var volumes={'a':'floppy_a','c':'main'};
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
+      throw {
+        name: 'FSErrno',
+        message:'ENODEV: '+path.slice(2)+': No such device'
+      }
+    }
+    fs[dev].deleteFile(
+      dev.slice(2),
+      dest.slice(2),
+    )
+  },
+  rmdir:async function(path,dest){
+    var volumeletter=path[0];
+    var AllowedVolumes=['a', 'c'];
+    var volumes={'a':'floppy_a','c':'main'};
+    var dev=fs.resolveByPrefix(volumeletter.toLowerCase()+":");
+    if(!dev){
+      throw {
+        name: 'FSErrno',
+        message:'ENODEV: '+path.slice(2)+': No such device'
+      }
+    }
+    fs[dev].deleteDir(
+      dev.slice(2),
+      dest.slice(2),
+    )
+  },
+  umount:function(prefix){
+    setTimeout(function(){
+    var dev=fs.resolveByPrefix(prefix.toLowerCase());
+    if(fs[dev]){fs[dev]=undefined;delete fs[dev]}
+    },100);
+  }
 };
 fakewin.StandardWindow=function(args){var msw=fakewin.util.Swgen(new StandardWindow(args.title,args.initialWidth,args.initialHeight,args.initialX,args.initialY,args.resizable,args.minHeight,args.minWidth,args.taskbar,"A random v3 app on v1 mwa",false));msw.setHtml(args.body);return msw;}
 fakewin.WindowSystem={
@@ -197,7 +604,7 @@ fakewin.util.Swgen=function(rsw){
     $sw:rsw,
     toString: function (){return "[object StandardWindow]"},
     setTitle: function(t){this.$sw.setTitle(t)},
-    close:function(){this.$sw.close()},
+    close:function(){rsw.close()},
     toggleMaximize:function(){this.$sw.toggleMaximize()},
     toggleMinimize:function(){this.$sw.toggleMinimize()},
     show:function(){this.$sw.show()},
@@ -214,31 +621,32 @@ fakewin.util.Swgen=function(rsw){
     setSize:function(w,h){this.$sw.setSize(w,h);},
     setWindowIcon:function(url){this.$sw.setWindowIcon(url,true);},
     showTitlebarMenu:function(){(function(){}).constructor("return window.w96")().ui.commdlg.msgboxSimple.error('bruh','bruh you should know v1 doesn\'t have titlebar menu', 'wat');},
-    params:{title:'a title',body:'bruh do you really need this',get initialHeight(){return rsw.initialHeight},get initialWidth(){return rsw.initialWidth},get initialX(){return rsw.initialX},get initialY(){return rsw.initialY},get taskbar(){return rsw.usesAppbar}}
+    params:{title:'a title',body:'bruh do you really need this',get initialHeight(){return rsw.initialHeight},get initialWidth(){return rsw.initialWidth},get initialX(){return rsw.initialX},get initialY(){return rsw.initialY},get taskbar(){return rsw.usesAppbar}},
+    getBodyContainer:function(){return this.wndObject.querySelector(".window-html-content");}
   };
   return sw;
 };
   
   var $$USEWRT$$=confirm('Run as wrt?')
-  
   if($$USEWRT$$){
     var $$ARGSSTR$$=prompt("Arguments (split by space)");
-    $$ARGSSTR$$=prompt("Command name:")+" "+$$ARGSSTR$$;
+    selfCmd=prompt("Command name:");
+    $$ARGSSTR$$=selfCmd+" "+$$ARGSSTR$$;
     var $$ARGS$$=$$ARGSSTR$$.split(" ");
-    var f$=eval(`(async function(w96,WApplication){
-${fs.main.getFileContents(fp)}
+    var f$=eval(`(async function(w96,WApplication,StandardWindow,FS,alert){
+${await fakewin.FS.readstr(fp)}
 })`);
 f$.call({
   boxedEnv:{args:$$ARGS$$}
-},fakewin,fakewin.WApplication).then(console.info).catch(function(e){
+},fakewin,fakewin.WApplication,fakewin.StandardWindow,fakewin.FS,V3Alert).then(console.info).catch(function(e){
   alert(e.name+":"+e.message);
 });
   }else{
 
-var f$=eval(`(async function(w96,WApplication){
-${fs.main.getFileContents(fp)}
+var f$=eval(`(async function(w96,alert,WApplication,StandardWindow,FS){
+${await fakewin.FS.readstr(fp)}
 })`);
-f$(fakewin).then(console.info).catch(function(e){
+f$(fakewin,V3Alert).then(console.info).catch(function(e){
   alert(e.name+":"+e.message);
 });
   
